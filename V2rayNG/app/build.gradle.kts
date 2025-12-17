4ceef3e35aea92ff508dd903d3029a458fd0229d
zhaoyuheqing <proooprop@outlook.com>
2025-12-17 14:56:03 +0800

    Update build.gradle.kts

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.v2ray.ang"
+        applicationId = "com.nn.ang"
         minSdk = 21
         targetSdk = 35
         versionCode = 683
