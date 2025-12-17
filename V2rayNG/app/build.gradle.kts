ad93d92aa4c99e4392c4f4387c6affe30759329e
zhaoyuheqing <proooprop@outlook.com>
2025-12-17 13:41:13 +0800

    Update build.gradle.kts

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.v2ray.ang"
+    namespace = "com.theme.ang"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.v2ray.ang"
+        applicationId = "com.theme.ang"
         minSdk = 21
         targetSdk = 35
         versionCode = 683
